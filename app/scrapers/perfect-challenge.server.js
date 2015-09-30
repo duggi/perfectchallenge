9ad0c647cba32e370be81dd8c7728c1e32a569cb
Jesse Hull <jessehull@gmail.com>
2015-09-30 12:38:13 -0700

    removed rusty

--- app/scrapers/perfect-challenge.server.js
+++ app/scrapers/perfect-challenge.server.js
@@ -269,7 +269,7 @@ var PLAYER_PHONES = {
 	'80\'s Spaceman' : '+15109674275', //jesse
 	'Cleveland Steamers' : '+14156139871', //dennis
 	'Deez Nuts for Prez' : '+16505761388', //doug
-	'Terrible Towelies' : '+14157229175', //rusty
+//	'Terrible Towelies' : '+14157229175', //rusty
 	'Better than Quidditch' : '+14159718315', //ian
 	'powderprincess57\'s picks' : '+16502192701', //amy
 	'Team Aloha' : '+14159729894' //sam
